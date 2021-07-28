44c15ee3cb258aebca04b7039b3466ea01c13686
DanielK2312 <kosmindaniel@gmail.com>
2021-07-28 12:50:06 -0400

    container around home buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,36 +4,38 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div
-      className="btns ui buttons"
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "200px",
-      }}
-    >
-      <a
-        style={{ borderStyle: "solid", borderWidth: "1px" }}
-        className="button-ele ui button ui inverted primary button"
-        href="/workout"
+    <div className="ui container">
+      <div
+        className="btns ui buttons"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "200px",
+        }}
       >
-        Generate Workout
-      </a>
-      <a
-        style={{ borderStyle: "solid", borderWidth: "1px" }}
-        className="button-ele ui button ui inverted primary button"
-        href="/searchExercise"
-      >
-        Search Exercise
-      </a>
-      <a
-        style={{ borderStyle: "solid", borderWidth: "1px" }}
-        className="button-ele ui button ui inverted primary button"
-        href="/dailyIntake"
-      >
-        Calculate Daily Intake
-      </a>
+        <a
+          style={{ borderStyle: "solid", borderWidth: "1px" }}
+          className="button-ele ui button ui inverted primary button"
+          href="/workout"
+        >
+          Generate Workout
+        </a>
+        <a
+          style={{ borderStyle: "solid", borderWidth: "1px" }}
+          className="button-ele ui button ui inverted primary button"
+          href="/searchExercise"
+        >
+          Search Exercise
+        </a>
+        <a
+          style={{ borderStyle: "solid", borderWidth: "1px" }}
+          className="button-ele ui button ui inverted primary button"
+          href="/dailyIntake"
+        >
+          Calculate Daily Intake
+        </a>
+      </div>
     </div>
   );
 };
