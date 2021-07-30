32dce05312df42a041998105c8f74c524f69c3c6
DanielK2312 <kosmindaniel@gmail.com>
2021-07-30 11:48:10 -0400

    Changed up button stlying along with adding directions to the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,42 +4,48 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="ui container">
-      <div
-        className="btns ui buttons"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "200px",
-        }}
-      >
-        <a
-          style={{ borderStyle: "solid", borderWidth: "1px" }}
-          className="button-ele ui button ui inverted primary button"
-          href="/workout"
-        >
-          Generate Workout
+    <div className="ui container" style={{ marginTop: "50px" }}>
+      <div className="three ui blue basic buttons">
+        <a className="ui red button" href="/workout">
+          Get Workout
         </a>
-        <a
-          style={{ borderStyle: "solid", borderWidth: "1px" }}
-          className="button-ele ui button ui inverted primary button"
-          href="/searchExercise"
-        >
-          Search Exercise
+        <a className="ui button" href="/searchExercise">
+          Lookup
         </a>
-        <a
-          style={{ borderStyle: "solid", borderWidth: "1px" }}
-          className="button-ele ui button ui inverted primary button"
-          href="/dailyIntake"
-        >
+        <a className="ui button" href="dailyIntake">
           Calculate Daily Intake
         </a>
+      </div>
+
+      <div style={{ marginTop: "50px", fontSize: "18px" }}>
+        <p>Welcome to my custom React application!</p>
+        <div>
+          <p>With this application, you are able to do one of three things:</p>
+          <ol>
+            <li>
+              Generate a Workout of your Choice targeting a specific body part
+              (Chest, Back, Legs) using the "Get Workout" Button
+            </li>
+            <li>
+              Loop up any exercises you are not familiar with using the "Lookup"
+              Button
+            </li>
+            <li>
+              Calculate the ideal number of calories you should be eating per
+              day to either lose, gain, or maintain weight for optimal
+              performance using the "Calculate Daily Intake" button
+            </li>
+          </ol>
+        </div>
+
+        <p>
+          The application is still in development and will have more workouts
+          able to be generated in the future
+        </p>
+        <p>Click any of the buttons above to get started!</p>
       </div>
     </div>
   );
 };
 
 export default Home;
-
-// changes
