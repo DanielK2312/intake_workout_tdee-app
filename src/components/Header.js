ac0a2841054c3eee05dd62e7b0aec848c9193a54
DanielK2312 <kosmindaniel@gmail.com>
2021-07-30 14:44:49 -0400

    nav bar fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,19 +5,38 @@ import "./styles.css";
 
 const Header = () => {
   return (
-    <div className="ui menu">
-      <a href="/" className="item">
-        Home
-      </a>
-      <a href="/workout" className="item">
-        Generate Workout
-      </a>
-      <a href="/searchExercise" className="item">
-        Search Exercise
-      </a>
-      <a href="/dailyIntake" className="item">
-        Calculate Daily Intake
-      </a>
+    <div className="ui container">
+      <div className="ui blue four item inverted menu">
+        <a href="/" className="header item">
+          Home
+        </a>
+        <a
+          href="/workout"
+          className={
+            window.location.pathname === "/workout" ? "active item" : "item"
+          }
+        >
+          Generate Workout
+        </a>
+        <a
+          href="/searchExercise"
+          className={
+            window.location.pathname === "/searchExercise"
+              ? "active item"
+              : "item"
+          }
+        >
+          Search Exercise
+        </a>
+        <a
+          href="/dailyIntake"
+          className={
+            window.location.pathname === "/dailyIntake" ? "active item" : "item"
+          }
+        >
+          Calculate Daily Intake
+        </a>
+      </div>
     </div>
   );
 };
